7a8f9111da3798e8ad1fab004d8fa3bccf34d482
Sandiroc <bsandilya1908@gmail.com>
2024-02-03 20:45:43 -0500

    text edit

--- something/ValentineDay2024/resources/script.js
+++ something/ValentineDay2024/resources/script.js
@@ -5,7 +5,7 @@ const noBtn = document.querySelector('.no-btn');
 const wrapperRect = wrapper.getBoundingClientRect();
 const noBtnRect = noBtn.getBoundingClientRect();
 yesBtn.addEventListener('click', () => {
-    question.innerHTML = ':3 (try clicking the no button)';
+    question.innerHTML = 'Enjoy your Lego flowers cutie <3';
 });
 noBtn.addEventListener('mouseover', () => {
     const i = Math.floor(Math.random() * (wrapperRect.width - noBtnRect.width)) + 1;
